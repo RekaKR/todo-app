a823402d4f775063fbb248ef7f911857ce68b1d6
RekaKR <RekaKR@users.noreply.github.com>
2021-01-26 22:41:51 +0100

    Add AddDash.jsx, edit App.jsx & Dashboard.jsx

--- todo-app/src/components/App.jsx
+++ todo-app/src/components/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import '../style/app.css';
 import Dashboard from './Dashboard';
+import AddDash from './AddDash.jsx';
 
 function App() {
   return (
     <div className="app">
-      Random text.
       <Dashboard />
+      <AddDash />
     </div>
   );
 }
